d70b495a0ae72a2d3de904934eed7f92f414bdd0
saxrick <artemsofin12@gmail.com>
2024-05-15 17:04:54 +0500

    fix body page

--- inpad_visuals/src/components/Body.jsx
+++ inpad_visuals/src/components/Body.jsx
@@ -1,15 +1,7 @@
-import tanya from '/tanya.jpg'
-import pidor from '/pidor.jpg'
-import realPidor from '/realPidor.jpg'
-import bindera from '/bindera.jpg'
-import zeburashka from '/zeburashka.jpg'
-import panin from '/panin.jpg'
-import realPilot from '/realPilot.jpg'
-import pidorSyadet from '/pidorSyadet.jpg'
 export default function Body() {
     return (
          <body>
-         
+
          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
          </body>

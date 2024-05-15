764996128d76d0bf598850a38f042525259e2d97
saxrick <artemsofin12@gmail.com>
2024-05-15 17:03:50 +0500

    fix body page

--- inpad_visuals/src/components/Body.jsx
+++ inpad_visuals/src/components/Body.jsx
@@ -9,18 +9,7 @@ import pidorSyadet from '/pidorSyadet.jpg'
 export default function Body() {
     return (
          <body>
-
-         {/*<a href="https://sun9-46.userapi.com/impg/EV6B5lgQNYa3TjMTpQv0mpRqJuF7EtYnH0XgeA/5GtsMv6gD7s.jpg?size=997x590&quality=96&sign=7eb468c9d0bdc55ed20c35d32107b238&type=album">*/}
-         {/*    <img className="tanya" src={realPilot} alt='aboba'/>*/}
-         {/*    <img className="tanya" src={realPidor} alt='aboba'/>*/}
-         {/*    <img className="tanya" src={pidor} alt='aboba'/>*/}
-         {/*    <img className="tanya" src={bindera} alt='aboba'/>*/}
-         {/*    <img className="tanya" src={zeburashka} alt='aboba'/>*/}
-         {/*    <img className="tanya" src={panin} alt='aboba'/>*/}
-         {/*    <img className="tanya" src={tanya} alt='aboba'/>*/}
-         {/*    <img className="tanya" src={pidorSyadet} alt='aboba'/>*/}
-         {/*</a>*/}
-
+         
          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
          </body>

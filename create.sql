9e019bc3ead78747f8c223b7805ffd770bbca864
saxrick <artemsofin12@gmail.com>
2024-05-15 17:00:32 +0500

    clean public add create script

---
+++ create.sql
@@ -0,0 +1,30 @@
+-- drop table users;
+-- drop table projects cascade;
+-- drop table user_project;
+-- select * from user_project
+
+create table users (
+	id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+	username varchar(255),
+	state bool,
+	login varchar(255),
+	password varchar(255),
+    role varchar(255),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE projects (
+	id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+	projectname varchar(255),
+	state bool,
+    projectinfo varchar(255),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE user_project (
+	user_id int NOT NULL,
+	project_id int NOT NULL,
+	PRIMARY KEY (user_id, project_id),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (project_id) REFERENCES projects(id)
+);
